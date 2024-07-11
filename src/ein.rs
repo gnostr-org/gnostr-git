647f00ecfa8765a285717ecd8ed0e70365a89971
randymcmillan <randymcmillan@protonmail.com>
2024-07-10 22:16:16 -0400

    Cargo.toml:src/ein.rs src/gix.rs: building MacOS
    NOTE: requires gnostr/ext/curl-8.5.0 built for MacOS 13.0
          on this machine

--- src/ein.rs
+++ src/ein.rs
@@ -1,7 +1,7 @@
 #![deny(rust_2018_idioms, unsafe_code)]
-
+use gnostr_git::porcelain;
 fn main() -> anyhow::Result<()> {
-    gitoxide::porcelain::main()
+    porcelain::main()
 }
 
 #[cfg(not(feature = "pretty-cli"))]
